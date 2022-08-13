c6da16aa6c3d47a4f8c5411f021ed9e6f8950806
Khan <nguyenhoangkhan210@gmail.com>
2022-08-13 21:03:28 +0700

    fix 3

--- routers/posts.route.js
+++ routers/posts.route.js
@@ -3,7 +3,7 @@ import { postsController } from "../app/controllers/postsController.js";
 const router = express.Router();
 
 router.post("/create", postsController.createPost);
-router.delete("/delete/:id", postsController.deletePost);
+router.delete("/delete", postsController.deletePost);
 router.post("/update", postsController.updatePost);
 router.get("/", postsController.getPost);
 
