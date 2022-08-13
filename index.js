eeceadbcea3536e9cc44511859ea536518af80a8
Khan <nguyenhoangkhan210@gmail.com>
2022-08-13 11:29:03 +0700

    better it

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ mongoose
   .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to DB");
-    app.listen(PORT, () => {
-      console.log("Server is running on port " + PORT);
-    });
   })
   .catch((err) => console.log("err", err));
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
