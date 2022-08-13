339b7ea520c703e3f83e3184c4a27a0d54081729
Khan <nguyenhoangkhan210@gmail.com>
2022-08-13 12:53:23 +0700

    first

--- routers/index.js
+++ routers/index.js
@@ -2,9 +2,6 @@ import postsRouter from "./posts.route.js";
 import siteRouter from "./site.route.js";
 const route = (app) => {
   app.use("/posts", postsRouter);
-  app.use("/ok", (req, res) => {
-    res.send("ok");
-  });
   app.use("/", siteRouter);
 };
 
