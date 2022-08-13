d31f001b97f5abd853494f8e0866778f4c7d552e
Khan <nguyenhoangkhan210@gmail.com>
2022-08-13 13:11:23 +0700

    fix

--- routers/posts.route.js
+++ routers/posts.route.js
@@ -2,8 +2,8 @@ import express from "express";
 import { postsController } from "../app/controllers/postsController.js";
 const router = express.Router();
 
-router.get("/", postsController.getPost);
 router.post("/create", postsController.createPost);
 router.post("/update", postsController.updatePost);
+router.get("/", postsController.getPost);
 
 export default router;
